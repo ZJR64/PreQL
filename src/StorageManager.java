07f521a896cf8f7a223d1955f05301aefe0e1708
JacksonOConnor <jackson26oconnor@gmail.com>
2023-02-22 15:31:30 -0500

    Committed stubbed out Storage Manager

---
+++ src/StorageManager.java
@@ -0,0 +1,49 @@
+package src;
+
+/**
+ * @author Jackson O'Connor jdo7339@rit.edu
+ */
+public class StorageManager {
+
+    /**
+     *
+     */
+    public void insert(){
+
+    }
+
+    /**
+     *
+     */
+    public void getRecord(){
+
+    }
+
+    /**
+     *
+     */
+    public void getPage(){
+
+    }
+
+    /**
+     *
+     */
+    public void getAllRecords(){
+
+    }
+
+    /**
+     *
+     */
+    public void deleteRecord(){
+
+    }
+
+    /**
+     *
+     */
+    public void updateRecord(){
+
+    }
+}
