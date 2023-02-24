84de5587d4c7318063a82f826a0bc79fbb8911e0
JacksonOConnor <jackson26oconnor@gmail.com>
2023-02-24 01:34:28 -0500

    Added function documentation to StorageManager

--- src/StorageManager.java
+++ src/StorageManager.java
@@ -8,42 +8,44 @@ package src;
 public class StorageManager {
 
     /**
-     * Attempts to insert a record into a table.
+     * Attempts to insert a record into the proper location in the given table.
+     * If a duplicate primary key is found, reports an error. If the table
+     * doesn't exist, creates the table.
      */
-    public static void insert(){
+    public void insert(){
 
     }
 
     /**
-     *
+     * Gets a record by using its primary key.
      */
     public void getRecord(){
 
     }
 
     /**
-     *
+     * Gets a page by it's table and page number.
      */
     public void getPage(){
 
     }
 
     /**
-     *
+     * Gets all records for a given table number.
      */
     public void getAllRecords(){
 
     }
 
     /**
-     *
+     * Deletes a record from a given table using the given primary key.
      */
     public void deleteRecord(){
 
     }
 
     /**
-     *
+     * Updates a record in a given table using the given primary key.
      */
     public void updateRecord(){
 
