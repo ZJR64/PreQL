017523d5072e877490de3d9cff2c3ef9d3a9da81
Nick Lewandowski <njl4420@rit.edu>
2023-03-09 00:42:05 -0500

    Remade the select all function to work with the new page and record classes. It is unable to get a page using the "getFileName" function as the file name.

--- src/StorageManager/StorageManager.java
+++ src/StorageManager/StorageManager.java
@@ -452,95 +452,15 @@ public class StorageManager {
         }
         System.out.println(makeAttributesString(table));                //print out the attributes
 
-        for(int i = 0; i < table.getPages(); i++){                      //for each page in order
-            byte[] page = bm.getPage(tableName, i);                     //gets the next page
-            System.out.println(page);                                      //TODO test this print
-
-
-            int numOfRecs = 0;                                          //Initialize the number of records
-            int index = 0;                                              //Index of the page
-            while (index < Integer.SIZE/8) {
-                numOfRecs = (numOfRecs << 8) + (page[index] & 0xFF);    //turns the first couple bytes into the number of records
-                index++;
-            }
-            index += Integer.SIZE/8;
-            for (int recordNum = 0; recordNum < numOfRecs; recordNum++){
-                //iterates through each record of the current page
-                String recordOutput = "| ";                             //Initialize the string of the record
-
-                for (Attribute att:table.getAttributes()) {
-
-                    int offset = 0;                                         //The offset of the
-                    while (index < Integer.SIZE/8) {
-                        offset = (offset << 8) + (page[index] & 0xFF);
-                        index++;
-                    }
-                    int size = 0;                                           //The size of the record
-                    while (index < Integer.SIZE/8) {
-                        size = (size << 8) + (page[index] & 0xFF);
-                        index++;
-                    }
-                    String type = att.getType();                                    //get the type of the attribute
-                    if (type.equals("integer")) {
-                        int intValue = 0;                                           //The size of the record
-                        for (int byteNum = 0; byteNum < Integer.SIZE/8; byteNum++) {
-                            intValue = (intValue << 8) + (page[offset + byteNum] & 0xFF);
-                        }
-                        String tempString = Integer.toString(intValue);
-                        tempString = String.format("%1$"+15+ "s", tempString);
-                        recordOutput = recordOutput + tempString + " | ";
-
-                    }
-                    else if (type.equals("double")) {
-                        byte[] bytes = new byte[Double.SIZE/8];
-                        double doubleValue = 0;                             //The size of the record
-                        for (int byteNum = 0; byteNum < Double.SIZE/8; byteNum++) {
-                            bytes[byteNum] = page[offset + byteNum];
-                        }
-                        int attributeSize = att.getSize();
-                        doubleValue = ByteBuffer.wrap(bytes).getDouble();
-                        String tempString = Double.toString(doubleValue);
-                        tempString = String.format("%1$"+20+ "s", tempString);
-                        recordOutput = recordOutput + tempString + " | ";
-
-                    }
-                    else if (type.equals("boolean")) {
-                        boolean boolValue = false;
-                        int temp = 0;
-                        temp = (page[offset] & 0xFF);
-                        boolValue = Boolean.valueOf(Integer.toString(temp));
-                        recordOutput = recordOutput + boolValue + " | ";
-
-                        if (boolValue == false){
-                            recordOutput = recordOutput + "false | ";
-                        }
-                        else{
-                            recordOutput = recordOutput + "true  | ";
-                        }
-
-                    }
-                    else if (type.startsWith("char")) {
-                        int charAmount = Integer.parseInt(type.substring(type.indexOf("(")+1, type.indexOf(")")).strip());
-                        String outputString = "";
-                        for (int byteNum = 0; byteNum < charAmount; byteNum++) {
-                            outputString = outputString +  (char) page[offset + byteNum];
-                        }
-                        recordOutput = recordOutput + outputString + " | ";
-
-                    }
-                    else if (type.startsWith("varchar")) {
-                        String outputString = "";
-                        for (int byteNum = 0; byteNum < size; byteNum++) {
-                            outputString = outputString +  (char) page[offset + byteNum];
-                        }
-                        recordOutput = recordOutput + outputString + " | ";
-
-                    }
+        ArrayList<Integer> pageList = table.getPageOrder();
+        for (Integer pgNum : pageList){
+            Page pg = new Page(pgNum, table, bm.getPageSize(), bm.getPage(table.getFileName(), pgNum));
+            for (Record rec : pg.getRecords()) {
+                for (String att: rec.getAttributes().keySet()) {
+                    System.out.println(rec.getAttributes().get(att) + "  |  ");
                 }
-                System.out.println(recordOutput + "\n");
-
+                System.out.println("\n");
             }
-
         }
 
         return "SUCCESS";
