c65dbd7e6aeb207ad106d15fe0af615ffc4b2e69
Kaitlyn DeCola <kmd8594@rit.edu>
2023-04-04 11:08:21 -0400

    Changed select to parse for multiple tables, where, and orderby

--- src/Commands/Select.java
+++ src/Commands/Select.java
@@ -9,9 +9,12 @@ import src.StorageManager.StorageManager;
  * @author Kaitlyn DeCola kmd8594@rit.edu
  */
 public class Select extends Command {
-    // table name
-    private String name;
+    // table names
+    private String[] name;
     private StorageManager sm;
+    private WhereClause where;
+    private String orderBy;
+    private String[] columns;
 
     // default constructor
     public Select(String input, StorageManager sm){
@@ -26,11 +29,42 @@ public class Select extends Command {
     @Override
     public void parse(){
         try {
+            // initialize where and orderby as null
+            this.where = null;
+            this.orderBy = null;
             // split on the keyword from
             // should return ["select *", <name>]
             String[] split = input.split("(?i)from");
-            // remove semicolon and white space
-            this.name = split[1].replace(";", "").strip();
+            String cols = split[0];
+            cols = cols.replace("select", "").strip();
+            this.columns = cols.split("\\s*,\\s*");
+            String splitTable = "";
+
+            // contains where clause
+            if(split[1].toLowerCase().contains("where")){
+                String[] splitWhere = split[1].split("(?i)where");
+                splitTable = splitWhere[0].strip();
+                // if there's an orderby, split again, otherwise the rest is the where clause
+                if(splitWhere[1].toLowerCase().contains("orderby")){
+                    String[] splitOrderBy = splitWhere[1].split("(?i)orderby");
+                    this.where = new WhereClause(splitOrderBy[0].strip());
+                    this.orderBy = splitOrderBy[1].replace(";","").strip();
+                }
+                else{
+                    this.where = new WhereClause(splitWhere[1].replace(";","").strip());
+                }
+            }
+            // doesn't contain where but contains orderby
+            else if(split[1].toLowerCase().contains("orderby")){
+                String[] splitOrderBy = split[1].split("(?i)orderby");
+                splitTable = splitOrderBy[0].strip();
+                this.orderBy = splitOrderBy[1].replace(";","").strip();
+            }
+            // doesn't contain where or orderby
+            else{
+                splitTable = split[1].replace(";", "").strip();
+            }
+            this.name = splitTable.split(", ");
             this.success = true;
         }
         catch(Exception e){
@@ -41,6 +75,6 @@ public class Select extends Command {
 
     @Override
     public String execute() {
-        return sm.getAllRecords(name);
+        return null;//sm.getAllRecords(name);
     }
 }
