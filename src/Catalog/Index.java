5e23c74dacec87219defc8e03b95057431b78602
zjr6302 <zjr6302@rit.edu>
2023-04-19 12:27:58 -0400

    Added basic object for index

---
+++ src/Catalog/Index.java
@@ -0,0 +1,21 @@
+package src.Catalog;
+
+public class Index {
+
+    public Index() {
+        //TODO constructor for index
+    }
+
+    public void addIndex(Object primaryKeyValue) {
+        //TODO add an index to the tree
+    }
+
+    public void removeIndex(Object primaryKeyValue) {
+        //TODO remove an index from the tree
+    }
+
+    public int findIndex(Object primaryKeyValue) {
+        //TODO find the page of the given primaryKey
+        return -1;
+    }
+}
